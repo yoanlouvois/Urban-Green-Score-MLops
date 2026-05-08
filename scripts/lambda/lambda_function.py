281a0e786cf47600678da5874135db8ff5ff5a4e
yoanlouvois <louvois45@gmail.com>
2026-05-08 16:42:21 +0200

    add: implement AWS Lambda function for invoking SageMaker endpoint

---
+++ scripts/lambda/lambda_function.py
@@ -0,0 +1,43 @@
+import os 
+import json 
+import base64
+import boto3
+
+runtime = boto3.client("sagemaker-runtime")
+
+ENDPOINT_NAME = os.getenv(
+    "SAGEMAKER_ENDPOINT_NAME",
+    "urban-green-score-endpoint"
+)
+
+def lambda_handler(event, context):
+    try:
+        body = event["body"]
+
+        # API Gateway envoie souvent le body encodé
+        if event.get("isBase64Encoded", False):
+            image_bytes = base64.b64decode(body)
+        else:
+            image_bytes = body.encode()
+        
+        response = runtime.invoke_endpoint(
+            EndpointName=ENDPOINT_NAME,
+            ContentType="image/png",
+            Body=image_bytes,
+        )
+
+        result = response["body"].read().decode("utf-8")
+
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": result,
+        }
+    
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)}),
+        }
+    
+    
