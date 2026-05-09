32ec914d4306623590dc301cebbd00bb53c4ecb8
yoanlouvois <louvois45@gmail.com>
2026-05-09 17:55:35 +0200

    add: create Terraform variables for AWS configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+    type = string
+    default = "eu-west-3"
+}
+
+variable "project_name" {
+    type = string
+    default = "urban-green-score"
+}
+
+variable "sagemaker_endpoint_name" {
+    type = string
+    default = "urban-green-score-endpoint"
+}
+
+variable "lambda_zip_path" {
+    type = string
+    default = "../lambda/lambda.zip"
+}
