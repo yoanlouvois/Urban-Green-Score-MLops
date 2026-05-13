836cf2280f65248b256d0628803a614ac40164e1
yoanlouvois <louvois45@gmail.com>
2026-05-14 00:40:28 +0200

    feat: add public access block configuration for S3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,3 +9,12 @@ resource "aws_s3_bucket" "ml_bucket" {
     Name = "urban-green-score"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "ml_bucket_block" {
+  bucket = aws_s3_bucket.ml_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
